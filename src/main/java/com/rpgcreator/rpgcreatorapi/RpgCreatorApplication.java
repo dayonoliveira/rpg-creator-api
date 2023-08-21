a9a75a53b6ae7230dfe42defe8b982c52527aafe
Dayon Oliveira <dayon.dos@gmail.com>
2023-08-21 07:41:47 -0300

    feat: first commit

---
+++ src/main/java/com/rpgcreator/rpgcreatorapi/RpgCreatorApplication.java
@@ -0,0 +1,14 @@
+package com.rpgcreator.rpgcreatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgCreatorApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpgcreator/rpgcreatorapi/RpgCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpgcreator.rpgcreatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
